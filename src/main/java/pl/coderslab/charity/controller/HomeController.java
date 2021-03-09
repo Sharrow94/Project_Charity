7f7d977a8b0bf6d3dfd07487d12c946534a4fe5a
Maciej Roszyk <maciej.roszyk@gmail.com>
2021-03-09 14:08:54 +0100

    MR/SumBags_and_sumDonations_ACTIONS

---
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -0,0 +1,28 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
+
+
+@Controller
+public class HomeController {
+
+    private final InstitutionService institutionService;
+    private final DonationService donationService;
+
+    public HomeController(InstitutionService institutionService, DonationService donationService) {
+        this.institutionService = institutionService;
+        this.donationService = donationService;
+    }
+
+    @RequestMapping("/")
+    public String homeAction(Model model){
+        model.addAttribute("institutions",institutionService.getAll());
+        model.addAttribute("bagsSum",donationService.getSumBags());
+        model.addAttribute("countDonations",donationService.countDonations());
+        return "index";
+    }
+}
