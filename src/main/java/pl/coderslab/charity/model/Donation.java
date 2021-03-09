7cca62001b72d917e8acc689d37e41bdc272cbe0
Maciej Roszyk <maciej.roszyk@gmail.com>
2021-03-09 14:07:04 +0100

    MR/Entity

---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,28 @@
+package pl.coderslab.charity.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    @OneToMany
+    private List<Category> categories;
+    @ManyToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate picUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+}
